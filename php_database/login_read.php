a23a030dae539f608948954cb2345fc08e0b4887
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-07-23 16:50:08 +0100

    first commit

---
+++ php_database/login_read.php
@@ -0,0 +1,46 @@
+<?php 
+    $database = mysqli_connect("localhost", "root", "", "login_app");
+
+    if($database) {
+        echo "connected";
+    }
+    else {
+        die("Connection failed");
+    }
+
+    $query = "SELECT * FROM users";
+
+    $result = mysqli_query($database, $query);
+
+    if(!$result) {
+        die("Query FAILED" . mysqli_error());
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Login Read</title>
+</head>
+<body>
+    <div class="card text-white bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+            <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <pre>
+                <?php
+                print_r($row);
+                ?>
+                </pre>
+                <?php
+            }   
+            ?>
+        </div>
+    </div>
+</body>
+</html>

2a9b09901db8344fc8c68451a5775777f3dae9df
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-07-29 22:23:06 +0100

    php and the web

---
+++ php_and_web/get.php
@@ -0,0 +1,20 @@
+<?php 
+    print_r($_GET);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+<?php
+    $id = 10;
+    $button = "CLICK";
+?>
+
+   <a href="get.php?id=<?php echo $id;?>"><?php echo $button;?></a>
+</body>
+</html>
